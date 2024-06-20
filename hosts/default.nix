54df31f3363654a8e04a8a99325f410fc8b82b05
comfybyte <comfybyte@proton.me>
2024-06-20 17:53:30 -0300

    hosts: improve code legibility

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,18 +1,22 @@
-inputs@{ nixpkgs, ... }: {
-  kirisame = nixpkgs.lib.nixosSystem (let
-    system = "x86_64-linux";
-    stable = import inputs.stable {
+inputs@{ nixpkgs, ... }:
+let
+  stableFor = system:
+    import inputs.stable {
       inherit system;
       config.allowUnfree = true;
     };
-    homeModulesWith = specialArgs: {
-      imports = [ inputs.home.nixosModules.home-manager ];
-      home-manager = {
-        extraSpecialArgs = specialArgs;
-        useGlobalPkgs = true;
-        useUserPackages = true;
-      };
+  homeModulesWith = specialArgs: {
+    imports = [ inputs.home.nixosModules.home-manager ];
+    home-manager = {
+      extraSpecialArgs = specialArgs;
+      useGlobalPkgs = true;
+      useUserPackages = true;
     };
+  };
+in {
+  kirisame = nixpkgs.lib.nixosSystem (let
+    system = "x86_64-linux";
+    stable = stableFor system;
     specialArgs = { inherit inputs system stable; };
   in {
     inherit specialArgs;
